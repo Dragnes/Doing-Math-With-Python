095f341bfe04c1493aed897f6dc8caf0ad418037
Dhruv Sengar <sengardhruv101@gmail.com>
2018-05-21 10:06:54 -0400

    Update Doing Math with Python CH 3 pg 61-91.py

--- Doing Math with Python CH 3 pg 61-91.py
+++ Doing Math with Python CH 3 pg 61-91.py
@@ -187,3 +187,91 @@ if __name__ == '__main__':
     print('The variance of the list of numbers is {0}'.format(variance))
     std = variance**0.5
     print('The standard deviation of the list of numbers is {0}'.format(std))
+
+ 
+# Calculating the Correlation Between Two Data Sets
+
+# Calculating the Correlation Coefficient
+
+simple_list1 = [1,2,3]
+simple_list2 = [4,5,6]
+for x, y in zip(simple_list1, simple_list2):
+    print(x, y)
+
+'''
+Calculat the correlation coefficient
+'''
+def find_corr_x_y(x, y):
+    n = len(x)
+    # Find the sum of the products
+    prod = []
+    for xi, yi in zip(x, y):
+        prod.append(xi*yi)
+    sum_prod_x_y = sum(prod)
+    sum_x = sum(x)
+    sum_y = sum(y)
+    squared_sum_x = sum_x**2
+    squared_sum_y = sum_y**2
+    x_square = []
+    for xi in x:
+        x_square.append(xi**2)
+    # Find the sum
+    x_square_sum = sum(x_square)
+    y_square = []
+    for yi in y:
+        y_square.append(yi**2)
+    # Find the sum
+    y_square_sum = sum(y_square)
+    # use formula from text to calculate correlation
+    numerator = n*sum_prod_x_y - sum_x*sum_y
+    denominator_term1 = n*x_square_sum - squared_sum_x
+    denominator_term2 = n*y_square_sum - squared_sum_y
+    denominator = (denominator_term1*denominator_term2)**0.5
+    correlation = numerator/denominator
+    return(correlation)
+
+# High School Grades and Performance on College Adimission Tests (examples)
+
+def find_corr_x_y(x, y):
+    n = len(x)
+    prod = []
+    for xi, yi in zip(x, y):
+        prod.append(xi*yi)
+    sum_prod_x_y = sum(prod)
+    sum_x = sum(x)
+    sum_y = sum(y)
+    squared_sum_x = sum_x**2
+    squared_sum_y = sum_y**2
+    x_square = []
+    for xi in x:
+        x_square.append(xi**2)
+    x_square_sum = sum(x_square)
+    y_square = []
+    for yi in y:
+        y_square.append(yi**2)
+    y_square_sum = sum(y_square)
+    numerator = n*sum_prod_x_y - sum_x*sum_y
+    denominator_term1 = n*x_square_sum - squared_sum_x
+    denominator_term2 = n*y_square_sum - squared_sum_y
+    denominator = (denominator_term1*denominator_term2)**0.5
+    correlation = numerator/denominator
+    return(correlation)
+
+import matplotlib.pyplot as plt
+if __name__ == '__main__':
+    x = [90,92,95,96,87,87,90,95,98,96]
+    y = [85,87,86,97,96,88,89,98,98,87]
+    correlation = find_corr_x_y(x,y)
+    print('The Correlation Coefficient of High School Grades and Performance on College Adimission Tests is {0}'.format(correlation))
+    plt.scatter(x, y, marker = 'x')
+    plt.ylabel('College admission Test Scores')
+    plt.xlabel('High School Grades')
+
+if __name__ == '__main__':
+    x = [83,85,84,96,94,86,87,97,97,85]
+    y = [85,87,86,97,96,88,89,98,98,87]
+    correlation = find_corr_x_y(x,y)
+    print('The Correlation Coefficient of High School Grades and Performance on College Adimission Tests is {0}'.format(correlation))
+    plt.scatter(x, y, marker = 'x')
+    plt.ylabel('College admission Test Scores')
+    plt.xlabel('High School Grades')
